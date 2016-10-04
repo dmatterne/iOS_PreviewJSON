efff47defeef667f502d00b4c836af06dc7b3098
Nancy De Smet <ciek.eu@gmail.com>
2016-10-04 14:36:33 +0200

    data wordt opgehaald

---
+++ NetworkExample/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  NetworkExample
+//
+//  Created by Tywin Lannister on 04/10/16.
+//  Copyright © 2016 Training. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    var id : Int = 0
+    var name : String = ""
+    var email : String = ""
+
+}
